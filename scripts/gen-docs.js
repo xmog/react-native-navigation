b1983cf824e051002d6fa0ca2b2f3499bf7c82cc
Daniel Zlotin <zlotindaniel@gmail.com>
2018-02-18 14:53:50 +0200

    trying stuff

---
+++ scripts/gen-docs.js
@@ -0,0 +1,15 @@
+const exec = require('shell-utils').exec;
+
+run();
+
+function run() {
+  exec.execSync(`typedoc 
+  --excludeExternals 
+  --excludePrivate 
+  --mode modules
+  --entryPoint ./lib/src/Navigation
+  --out ./docs/temp/
+  ./lib/src
+  `);
+}
+// --json ./docs/typedoc.json
