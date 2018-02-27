d2ee75251e9cf8d2a7ac1823e6dec39205120098
Daniel Zlotin <zlotindaniel@gmail.com>
2018-02-27 15:12:54 +0200

    local-docs no cache

--- scripts/local-docs.js
+++ scripts/local-docs.js
@@ -7,7 +7,9 @@ run();
 function run() {
   const app = express();
   app.get('/', (req, res) => {
-    res.setHeader('Cache-Control', 'no-cache');
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    res.header('Expires', '-1');
+    res.header('Pragma', 'no-cache');
     res.sendFile(process.cwd() + '/docs/index.html');
   });
   app.listen(PORT);
