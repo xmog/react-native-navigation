1ffb2f384218c231ca2a28c912d7c1cea6f0c194
Daniel Zlotin <zlotindaniel@gmail.com>
2018-02-27 15:10:09 +0200

    auto docs

--- scripts/gen-docs.js
+++ scripts/gen-docs.js
@@ -1,9 +1,37 @@
+const _ = require('lodash');
+const path = require('path');
 const exec = require('shell-utils').exec;
+
+const reflectionsPath = path.resolve('./../docs/reflections/');
+
 run();
 
 function run() {
+  const modulePath = 'Navigation';
+
+  generateJsonReflections(modulePath);
   
+  const theModule = require(`${reflectionsPath}/${modulePath}`).children[0];
+  const theClass = theModule.children[0];
+  const methodIds = _.find(theClass.groups, (g) => g.title === 'Methods').children;
+
 }
+
+function generateJsonReflections(modulePath) {
+  exec.execSync(`typedoc
+  --excludeExternals
+  --excludePrivate
+  --excludeProtected
+  --includeDeclarations
+  --module commonjs
+  --readme none
+  --target ES6
+  --theme markdown
+  --json ${reflectionsPath}/${modulePath}.json
+  ./lib/src/${modulePath}.ts
+  `);
+}
+
 // exec.execSync(`typedoc
 // --excludeExternals
 // --excludePrivate
